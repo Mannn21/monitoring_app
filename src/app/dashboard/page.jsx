1d3ed7261c7b9065768a96f84a85d1b1868bc679
Mannn21 <aimannurrofi21@gmail.com>
2023-07-29 12:30:40 +0700

    create dashboard page

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,9 +1,36 @@
-const Page = () => {
-    return (
-        <div>
-            Dashboard Page....
-        </div>
-    )
-}
+import styled from "./page.module.css";
+import dayjs from "dayjs";
 
-export default Page
+const Page = () => {
+	const currentDate = dayjs();
+	const formattedDate = currentDate.locale("id").format("dddd, D MMMM YYYY");
+	return (
+		<div className={styled.container}>
+			<div className={styled.wrapper}>
+				<div className={styled.infoWrapper}>
+					<div className={styled.titleWrapper}>
+						<h1 className={styled.title}>My Schedule Today</h1>
+						<span className={styled.date}>{formattedDate}</span>
+					</div>
+					<div className={styled.statisticWrapper}>
+						<div className={styled.averageWrapper}>
+							<h2 className={styled.averageTitle}>Average Grade</h2>
+							<span className={styled.average}>85 / 100</span>
+						</div>
+						<div className={styled.averageWrapper}>
+							<h2 className={styled.averageTitle}>Average Attendance</h2>
+							<span className={styled.average}>100 / 100</span>
+						</div>
+					</div>
+				</div>
+				<div className={styled.listWrapper}></div>
+				<div className={styled.classWrapper}>
+					<div className={styled.subClassWrapper}></div>
+					<div className={styled.subClassWrapper}></div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Page;
