652e036a751a014ac03ddf7dbf171427493c6806
Mannn21 <aimannurrofi21@gmail.com>
2023-08-13 17:34:06 +0700

    update users

--- src/data/users.js
+++ src/data/users.js
@@ -8,6 +8,14 @@ export const Users = [
         phoneNumber: "012121313",
         createdAt: "June 2021",
         image: "Asep",
+        attendance: {
+            present: 3,
+            alpha: 1,
+            sick: 3,
+            permission: 3
+        },
+        grade: 68,
+        attitude: "B"
     },
     {
         id: 2,
@@ -18,6 +26,14 @@ export const Users = [
         phoneNumber: "02824672681",
         createdAt: "May 2021",
         image: "Kurnia",
+        attendance: {
+            present: 5,
+            alpha: 2,
+            sick: 0,
+            permission: 3
+        },
+        grade: 73,
+        attitude: "C"
     },
     {
         id: 3,
@@ -28,6 +44,14 @@ export const Users = [
         phoneNumber: "0263781263",
         createdAt: "June 2021",
         image: "Putri",
+        attendance: {
+            present: 2,
+            alpha: 2,
+            sick: 5,
+            permission: 1
+        },
+        grade: 35,
+        attitude: "C"
     },
     {
         id: 4,
@@ -38,6 +62,14 @@ export const Users = [
         phoneNumber: "013637313",
         createdAt: "April 2021",
         image: "Dadang",
+        attendance: {
+            present: 9,
+            alpha: 0,
+            sick: 1,
+            permission: 0
+        },
+        grade: 80,
+        attitude: "A"
     },
     {
         id: 5,
@@ -48,6 +80,14 @@ export const Users = [
         phoneNumber: "0786481265",
         createdAt: "July 2021",
         image: "Heni",
+        attendance: {
+            present: 2,
+            alpha: 2,
+            sick: 2,
+            permission: 4
+        },
+        grade: 48,
+        attitude: "C"
     },
     {
         id: 6,
@@ -58,6 +98,14 @@ export const Users = [
         phoneNumber: "072487928",
         createdAt: "July 2021",
         image: "Yani",
+        attendance: {
+            present: 5,
+            alpha: 1,
+            sick: 1,
+            permission: 3
+        },
+        grade: 70,
+        attitude: "B"
     },
     {
         id: 7,
@@ -68,6 +116,14 @@ export const Users = [
         phoneNumber: "0724897124",
         createdAt: "June 2021",
         image: "Hendra",
+        attendance: {
+            present: 10,
+            alpha: 0,
+            sick: 0,
+            permission: 0
+        },
+        grade: 96,
+        attitude: "A"
     },
     {
         id: 8,
@@ -78,6 +134,14 @@ export const Users = [
         phoneNumber: "0129748921",
         createdAt: "April 2021",
         image: "Junichi",
+        attendance: {
+            present: 8,
+            alpha: 0,
+            sick: 1,
+            permission: 1
+        },
+        grade: 84,
+        attitude: "B"
     },
     {
         id: 9,
@@ -88,6 +152,14 @@ export const Users = [
         phoneNumber: "0172894729",
         createdAt: "June 2021",
         image: "Goro",
+        attendance: {
+            present: 4,
+            alpha: 2,
+            sick: 3,
+            permission: 1
+        },
+        grade: 50,
+        attitude: "C"
     },
     {
         id: 10,
@@ -98,6 +170,14 @@ export const Users = [
         phoneNumber: "02467824682",
         createdAt: "June 2021",
         image: "Putri R",
+        attendance: {
+            present: 10,
+            alpha: 0,
+            sick: 0,
+            permission: 0
+        },
+        grade: 88,
+        attitude: "A"
     },
     {
         id: 11,
@@ -108,6 +188,14 @@ export const Users = [
         phoneNumber: "0765823789",
         createdAt: "July 2021",
         image: "Nisa",
+        attendance: {
+            present: 3,
+            alpha: 1,
+            sick: 3,
+            permission: 3
+        },
+        grade: 34,
+        attitude: "D"
     },
     {
         id: 12,
@@ -118,6 +206,14 @@ export const Users = [
         phoneNumber: "0723784567",
         createdAt: "June 2021",
         image: "Jana",
+        attendance: {
+            present: 4,
+            alpha: 3,
+            sick: 0,
+            permission: 3
+        },
+        grade: 58,
+        attitude: "B"
     },
     {
         id: 13,
@@ -128,5 +224,13 @@ export const Users = [
         phoneNumber: "0264732674",
         createdAt: "July 2021",
         image: "Rahmat",
+        attendance: {
+            present: 2,
+            alpha: 1,
+            sick: 2,
+            permission: 5
+        },
+        grade: 60,
+        attitude: "B"
     },
 ]
